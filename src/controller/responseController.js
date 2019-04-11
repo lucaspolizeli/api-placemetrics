41679dd8aa218d98160722a59eccc279824fa276
Lucas Polizeli <lumpolizeli@gmail.com>
2019-04-11 13:50:20 -0300

    Finalize api test

--- src/controller/responseController.js
+++ src/controller/responseController.js
@@ -11,8 +11,24 @@ router.post("/", async (req, res) => {
   }
 });
 
-router.get("/", (req, res) => {
-  res.send({ ok: true });
+router.get("/", async (req, res) => {
+  try {
+    const userData = await UserData.find();
+    res.send({ userData });
+  } catch (error) {
+    return res.status(400).send({ error: "Failed to load data." });
+  }
+});
+
+router.get("/:userIp", async (req, res) => {
+  try {
+    const userData = await UserData.find({ userIp: req.params.userIp });
+    if (!userData.length) return res.status(204);
+
+    res.send({ userData });
+  } catch (error) {
+    return res.status(400).send({ error: "Failed to load data." });
+  }
 });
 
 module.exports = app => app.use("/", router);
