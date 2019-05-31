964a889a232d2ab68ce1d2677f98170e0c04a4a0
mupapi <mupapi@hotmail.com>
2019-05-31 17:08:16 -0300

    Update apiController.js

--- src/controller/apiController.js
+++ src/controller/apiController.js
@@ -22,12 +22,71 @@ router.get("/", async (req, res) => {
 
 router.get("/:userIp", async (req, res) => {
   try {
-    const userData = await UserData.find({ userIp: req.params.userIp });
-    if (!userData.length) return res.status(204);
+    const userData = await UserData.find()
+                    .where('userIp')
+                    .equals(req.params.userIp)
+                    .select('_id temperature humidity userIp createdAt')
+                    .exec();
+    res.send({
+      userData
+    });
+  } catch (error) {
+    return res.status(400).send({
+      error: "Failed to load data."
+    });
+  }
+});
 
+router.get("since/:datetime", async (req, res) => {
+  try {
+    const userData = await UserData.find()
+                    .where('createdAt').gt(Date(req.params.datetime))
+                    .select('_id temperature humidity userIp createdAt')
+                    .exec();
+    if (!userData.length) 
+    return res.status(204);
+    
     res.send({ userData });
   } catch (error) {
     return res.status(400).send({ error: "Failed to load data." });
+  }
+});
+
+router.get("on/:date", async (req, res) => {
+  try {
+    const userData = await UserData.find().
+                    where('createdAt')
+                    .gt(Date(req.params.date).setHours(0, 0, 0, 0))
+                    .lt(Date(req.params.date).setHours(23, 59, 59, 999))
+                    select('_id temperature humidity userIp createdAt')
+                    exec();
+    if (!userData.length) 
+    return res.status(204);
+    
+    res.send({ userData });
+  } catch (error) {
+    return res.status(400).send({ error: "Failed to load data." });
+  }
+});
+
+router.get("/from/:startDate/to/:endDate", async (req, res) => {
+  try {
+    const userData = await UserData.find()
+                    .where('createdAt')
+                    .gt(Date(req.params.startDate))
+                    .lt(Date(req.params.endDate))
+                    .select('_id temperature humidity userIp createdAt')
+                    .exec();
+    if (!userData.length) 
+    return res.status(204);
+    
+    res.send({
+      userData
+    });
+  } catch (error) {
+    return res.status(400).send({
+      error: "Failed to load data."
+    });
   }
 });
 
